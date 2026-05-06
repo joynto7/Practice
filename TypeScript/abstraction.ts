54938094a5d5a104853ef4d2df444d4c9362bb97
joyntoghosh <joyntoghosh7@gmail.com>
2026-05-06 15:16:30 +0600

    using abstract class

--- TypeScript/abstraction.ts
+++ TypeScript/abstraction.ts
@@ -25,3 +25,33 @@ const myPlayer = new MusicPlayer();
 myPlayer.play();
 myPlayer.pause();
 myPlayer.stop();
+
+
+// uisng abstract class
+
+abstract class flower{
+   abstract smell():void;
+   abstract color():void;
+   abstract temarature():void
+}
+
+class rose extends flower{
+    smell(): void {
+        console.log('Good Smell');
+        
+    }
+    color(): void {
+        console.log('red');
+        
+    }
+    temarature(): void {
+        console.log('slightly clod');
+        
+    }
+
+}
+
+const flower1 = new rose()
+flower1.color();
+flower1.smell();
+flower1.temarature();
